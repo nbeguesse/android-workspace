21619c02119e10770c38f7786e80c2e7eed07603
Nicole Beguesse <nicole@highlinesale.com>
2014-09-15 15:51:37 -0400

    Adding PhotoGallery and Metadata

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/10/60bd0fded63700141b88e311fcf4c3fb
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class EditCrimeActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment(){
+		return new EditCrimeFragment();
+		
+	}
+
+
+}
---
+++ PhotoGallery/src/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+	public Fragment createFragment(){
+		return new PhotoGalleryFragment();
+	}
+}
